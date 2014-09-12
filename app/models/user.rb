58ff93f5dbfcb6e7e7d8a6f17346f222117abc91
Justin Speers <speersjustin@fastmail.fm>
2014-09-12 12:49:26 -0700

    added basic navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates_presence_of :handle, :on => [:update, :create]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+private
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'application#index'
+
+  # get 'profile', to: 'users#show'
+  # get 'login', to: user_session_path
+  # get 'logout', to: destroy_user_session_path
 end
