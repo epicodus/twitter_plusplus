3122f17a4958b75519ec147f6d8b4e1c79c71feb
Justin Speers <speersjustin@fastmail.fm>
2014-09-14 11:31:01 -0700

    create tweet model

---
+++ spec/tweet_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+
+describe Tweet do
+    it { should validate_presence_of :content }
+    it { should ensure_length_of(:content).is_at_least(1) }
+    it { should ensure_length_of(:content).is_at_most(141) }
+    
+
+end
