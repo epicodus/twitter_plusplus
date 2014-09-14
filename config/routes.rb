ad0311278417fc797e1d4bf03ed912fe79d6539f
Justin Speers <speersjustin@fastmail.fm>
2014-09-14 12:09:38 -0700

    add custom controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # resources :users, only: :show
+  resources :users, only: [:index, :show]
 
   root to: 'application#index'
 
   #todo remap user_root_path to redirect devise after login
-  
+
 end
