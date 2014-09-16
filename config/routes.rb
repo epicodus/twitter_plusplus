78ba90295fff0c3e8fd19f41737fd6ede747873c
Justin Speers <speersjustin@fastmail.fm>
2014-09-16 08:46:58 -0700

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users, only: [:index, :show]
+  #resources :users, only: [:index, :show]
   resource :tweets
 
   root to: 'application#index'
-
-  get 'timeline' => 'tweets#index', as: :user_root
+  get 'timeline', to: 'tweets#index', as: :user_root
   get 'follow/:id', to: 'relations#follow', as: :follow_user
-
-  #todo remap user_root_path to redirect devise after login
-
 end
