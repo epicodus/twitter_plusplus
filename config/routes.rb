4f7443172feb5bb1710c59f78c47fb6784ed1128
Justin Speers <speersjustin@fastmail.fm>
2014-09-16 20:48:38 -0700

    WIP must fix user show page follow/unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   get '/timeline/follow/:id', to: 'tweets#follow', as: :timeline_follow_user
   get '/timeline/unfollow/:id', to: 'tweets#unfollow', as: :timeline_unfollow_user
 
+  get '/user/follow/:id', to: 'users#follow', as: :user_follow
+  get '/user/unfollow/:id', to: 'users#unfollow', as: :user_unfollow
+
+
   get '/tweets/search', to:'tweets#search', as: :tweets_search
   get '/users/search', to:'users#search', as: :users_search
   get '/user/:id', to: 'users#show', as: :user_show
