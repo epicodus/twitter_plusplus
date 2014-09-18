eebeed2b53de05853c42a64e5157ec4e546619c5
Justin Speers <speersjustin@fastmail.fm>
2014-09-18 16:28:56 -0700

    fix ajax follow/unfollow from list follow(ing/ers) page

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -2,8 +2,6 @@ class RelationsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @followers = @user.followers
-    @following = @user.followed_users
   end
 
 
